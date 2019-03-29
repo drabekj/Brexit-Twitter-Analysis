55b3260eb49e0a7b3502a28c99adf26a3ee60d82
Jan Drabek <drabek.honza@gmail.com>
2019-03-29 16:54:04 +0100

    .gitignore fix

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # Twitter API keys
-CONSUMER_KEY = "YOUR_KEY"
-CONSUMER_SECRET = "YOUR_SECRET"
-OAUTH_TOKEN = "YOUR_ACCESS_TOKEN"
-OAUTH_TOKEN_SECRET = "YOUR_TOKEN_SECRET"
+CONSUMER_KEY = 'LEcqSAfZlov6hF0KDnrKs2GEB'
+CONSUMER_SECRET = 'ZS3xizSXHSh77luOzszHNImqRfPcCeD6pyfeHLSgNYzE8EJFwJ'
+OAUTH_TOKEN = '4733227402-CJovLe2KPNKcX03ePq0NCHI4cARyTCf5PgQ3Bze'
+OAUTH_TOKEN_SECRET = 'ggq1Q8qLLrsZppg9cjognwIcnzQyAfnXDOy53WooL0Bal'
 
 ## Watson API
 # Watson Tone Analyzer
-api_key_tone = 'IBM_WATSON_TONE_ANALYZER_API_KEY'
-service_url_tone = 'IBM_WATSON_TONE_ANALYZER_SERVICE_URL'
+api_key_tone = 'PZC-VjvPezBkZfEcOGVK8esCGpFh6FDWcWqkgQXxxRfS'
+service_url_tone = 'https://gateway-lon.watsonplatform.net/tone-analyzer/api'
 # Watson Natural Language Understanding
-api_key_keywords = 'IBM_WATSON_NLU_API_KEY'
-service_url_keywords = 'IBM_WATSON_NLU_SERVICE_URL'
+api_key_keywords = 'bx0Zum8pdNqkukMrACpppjBoKjrh7Mb56TXFOpcYfTMX'
+service_url_keywords = 'https://gateway-fra.watsonplatform.net/natural-language-understanding/api'
