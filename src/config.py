e99372da97d86b9dbb399bfb7f7b27a147480087
Jan Drabek <drabek.honza@gmail.com>
2019-03-29 14:56:54 +0100

    + Watson API keys

--- src/config.py
+++ src/config.py
@@ -3,3 +3,11 @@ CONSUMER_KEY = "YOUR_KEY"
 CONSUMER_SECRET = "YOUR_SECRET"
 OAUTH_TOKEN = "YOUR_ACCESS_TOKEN"
 OAUTH_TOKEN_SECRET = "YOUR_TOKEN_SECRET"
+
+## Watson API
+# Watson Tone Analyzer
+api_key_tone = 'IBM_WATSON_TONE_ANALYZER_API_KEY'
+service_url_tone = 'IBM_WATSON_TONE_ANALYZER_SERVICE_URL'
+# Watson Natural Language Understanding
+api_key_keywords = 'IBM_WATSON_NLU_API_KEY'
+service_url_keywords = 'IBM_WATSON_NLU_SERVICE_URL'
